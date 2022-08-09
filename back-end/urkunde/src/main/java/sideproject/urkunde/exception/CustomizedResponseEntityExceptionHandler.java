0977006a13bbbed3cdc0bee4fd2d2ea03b10919d
soochan <tncksdl05@gmail.com>
2022-08-10 00:13:18 +0900

    feat : add valid exception handler

--- back-end/urkunde/src/main/java/sideproject/urkunde/exception/CustomizedResponseEntityExceptionHandler.java
+++ back-end/urkunde/src/main/java/sideproject/urkunde/exception/CustomizedResponseEntityExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,9 +17,8 @@ import java.util.Date;
  * 모든 컨트롤러가 실행될때 반드시 해당 어노테이션을 갖고 있는 bean이 자동으로 실행 .
  * 에러가 발생한다고 하면 핸들러에서 등록시켰던 에러 메소드가 실행이될 수 있다.
  */
-@RestController
-@ControllerAdvice
-public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class CustomizedResponseEntityExceptionHandler  {
 
     /**
      * controller에서 일반 Exception이 발생하면 실행되는 method
@@ -50,20 +50,18 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     /**
      * 사용자가 잘못된 값을 넣어서 valid 문제가 생겼을때 처리 하는 메서드
      */
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public final ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex){
 
-        // 실제로 반환시 message를 ex.getMessage()하면 너무 길어져서 간단하게 저런식으로 작성하고 , 디테일한 에러 메시지는 3번째 파리미터에서
-        // 확인할 수 있도록 한다.
         ExceptionResponse exceptionResponse =
-                new ExceptionResponse(new Date(), "Validation Failed", ex.getBindingResult().toString());
+               new ExceptionResponse(new Date(), "Validation Failed",
+                                    ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST); // 400번 error
     }
+
+
 
 
 
