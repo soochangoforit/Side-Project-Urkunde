ed66a8ae8d5dc6ec38f840b637b2244c930395ea
soochan <tncksdl05@gmail.com>
2022-08-10 02:00:57 +0900

    feat : add QuizCheckRepository

--- back-end/urkunde/src/main/java/sideproject/urkunde/repository/QuizCheckRepository.java
+++ back-end/urkunde/src/main/java/sideproject/urkunde/repository/QuizCheckRepository.java
@@ -1,10 +1,17 @@
 package sideproject.urkunde.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sideproject.urkunde.domain.QuizCheck;
 
+import java.util.Optional;
+
 @Repository
 public interface QuizCheckRepository extends JpaRepository<QuizCheck, Long> {
+
+    @Query(value = "select qc from QuizCheck qc where qc.quiz.id= :quizId and qc.quizCycle.id = :quizCycleId")
+    Optional<QuizCheck> findByQuizAndQuizCycle(@Param("quizId") Long quizId,@Param("quizCycleId") Long quizCycle);
 
 }
